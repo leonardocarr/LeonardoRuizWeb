b8d3ef86c7e6d112a69aeb3791df3d27d7cd0c4e
LeonardoRuiz <leonardoruiz.carr@gmail.com>
2024-04-18 16:22:44 -0300

    update site

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-function typeWrite(elemento){
+function typeWrite(elemento) {
 	const textoArray = elemento.innerHTML.split('');
 	elemento.innerHTML = ' ';
-	textoArray.forEach(function(letra, i){   
-		
-	setTimeout(function(){
-			elemento.innerHTML += letra;
-	}, 75 * i)
+	textoArray.forEach(function (letra, i) {
 
-});
+		setTimeout(function () {
+			elemento.innerHTML += letra;
+		}, 75 * i)
+
+	});
 }
 const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
+
+
+// Search bar
+function aplicaFiltroCards() {
+	var input, filter, cards, cardContainer, h5, title, i;
+	input = document.getElementById("filtroCards");
+	filter = input.value.toUpperCase();
+	cardContainer = document.getElementById("containerCards");
+	cards = cardContainer.getElementsByClassName("card");
+	for (i = 0; i < cards.length; i++) {
+		title = cards[i].querySelector(".card-body");
+		if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+			cards[i].style.display = "";
+		} else {
+			cards[i].style.display = "none";
+		}
+	}
+}
